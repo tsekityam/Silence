cbad67f4d325e915603f2533312a78fa4f91e37f
Tse Kit Yam <me@kytse.com>
2016-08-17 23:30:26 +0800

    feat: update UI
    implement welcome screen and add setup instruction on main screen

--- Silence/ViewController.swift
+++ Silence/ViewController.swift
@@ -18,17 +18,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func openSettings(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: UIApplicationOpenSettingsURLString)!)
+    }
 
 }
 
