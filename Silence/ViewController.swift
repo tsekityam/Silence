0dd935bced5fa4f7385777b43068883da1f45d0c
Tse Kit Yam <me@kytse.com>
2016-08-17 23:06:53 +0800

    Initial Commit

---
+++ Silence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silence
+//
+//  Created by Tse Kit Yam on 17/8/2016.
+//  Copyright © 2016 Tse Kit Yam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
