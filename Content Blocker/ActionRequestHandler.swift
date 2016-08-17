70dadb6706945d9dde8656bdb6d8ab52ccb54fb1
Tse Kit Yam <me@kytse.com>
2016-08-17 23:08:42 +0800

    feat: add content blocker target

---
+++ Content Blocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  Content Blocker
+//
+//  Created by Tse Kit Yam on 17/8/2016.
+//  Copyright © 2016 Tse Kit Yam. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
